e45923b4163d834fdc8c88cc9338965060a65f86
dimas0309 <61481579+dimas0309@users.noreply.github.com>
2022-04-01 21:17:15 -0400

    first commit

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
 	host: 'localhost',
-	user: 'root',
-	password: 'dimas0309',
+	user: 'user',
+	password: 'userpassword',
 	database: 'invoicedb'
 })
 
