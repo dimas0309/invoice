62de04c123fb4c83bd569b9a6db461606b33de78
dimas0309 <61481579+dimas0309@users.noreply.github.com>
2022-04-04 20:40:21 -0400

    first commit

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
 	host: 'localhost',
-	user: 'user',
-	password: 'userpassword',
+	user: 'root',
+	password: 'dimas0309',
 	database: 'invoicedb'
 })
 
